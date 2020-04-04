aaaee0372e6435d1866c6249bf52a59516dbd9df
nathanlct <nathan.lichtle@gmail.com>
2020-04-04 21:57:12 +0200

    .

--- classify.py
+++ classify.py
@@ -36,7 +36,7 @@ print('Loaded model ' + model_path)
 def load_cloud(path):
     print('Loading point cloud at ' + path)
     data = read_ply(path)
-    print(list(data.keys()))
+    print(data)
     # values = data['label']
     points = np.vstack((data['x'], data['y'], data['z'])).T
     # pcd = np.array(o3d.io.read_point_cloud(path).points)
