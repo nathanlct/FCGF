48e4a884cf3a294326edd7bd5b9b2060b2efbea2
nathanlct <nathan.lichtle@gmail.com>
2020-04-12 18:01:52 +0200

    .

--- classify.py
+++ classify.py
@@ -6,6 +6,6 @@ names = ['Lille1_1', 'Lille1_2', 'Lille2', 'Paris1', 'Paris2']
 for s in names:
     feats = np.load(f'{s}_features.npy')
     lbs = np.load(f'{s}_labels.npy')
-    print(feats.shape, features.shape)
+    print(feats.shape)
     #features = np.vstack((features, feats))
     #labels = np.vstack((labels, lbs))
