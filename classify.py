458626fa47f99169075d48b886b3e41832c78b9f
nathanlct <nathan.lichtle@gmail.com>
2020-04-12 18:12:49 +0200

    .

--- classify.py
+++ classify.py
@@ -1,5 +1,19 @@
 import numpy as np
+import tensorflow as tf
 
+
+N_CLASSES = 10  # between 0 and 9
+
+model = tf.keras.Sequential([
+    tf.keras.layers.Dropout(0.3),
+    tf.keras.layers.Dense(128, activation='relu'),
+    tf.keras.layers.Dropout(0.3),
+    tf.keras.layers.Dense(N_CLASSES)
+])
+
+model.compile(optimizer=tf.keras.optimizers.RMSprop(),
+              loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
+              metrics=['sparse_categorical_accuracy'])
 
 names = ['Lille1_1', 'Lille1_2', 'Lille2', 'Paris1', 'Paris2']
 
@@ -8,7 +22,14 @@ for s in names:
     lbs = np.load(f'{s}_labels.npy')
     print(np.min(lbs), np.max(lbs))
     print(feats.shape)
+    
+    print('starting train on batch')
+    model.train_on_batch(feats, lbs)
+    print('done')
+
+
     #features = np.vstack((features, feats))
     #labels = np.vstack((labels, lbs))
+    
     del feats
     del lbs
