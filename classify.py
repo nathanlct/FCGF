1f4d6a645fdaa219cded1836114f17fca0514957
nathanlct <nathan.lichtle@gmail.com>
2020-04-12 18:39:04 +0200

    .

--- classify.py
+++ classify.py
@@ -1,8 +1,14 @@
 import numpy as np
 import tensorflow as tf
 
+use_small_data = True
 
-N_CLASSES = 7 #10  # between 0 and 9
+if use_small_data:
+    N_CLASSES = 7
+    names = ['MiniLille1', 'MiniLille2', 'MiniParis']
+else:
+    N_CLASSES = 10  # between 0 and 9
+    names = ['Lille1_1', 'Lille1_2', 'Lille2', 'Paris1', 'Paris2']
 
 model = tf.keras.Sequential([
     tf.keras.layers.Dropout(0.3),
@@ -16,52 +22,23 @@ model.compile(optimizer=tf.keras.optimizers.RMSprop(),
               metrics=['sparse_categorical_accuracy'])
 
 
-def generate_data():
-    while True:
-        feats = np.load(f'MiniLille1_features.npy')
-        lbs = np.load(f'MiniLille2_labels.npy')
+if use_small_data:
+    features = np.load(f'{names[0]}_features.npy')
+    labels = np.load(f'{names[0]}_labels.npy')
 
-        # shuffle data
-        inds = np.random.shuffle(list(range(len(feats))))
-        feats = feats[inds]
-        lbs = lbs[inds]     
-        
-        bs = 64
-        for i in range(len(feats) // bs):
-            yield (feats[i*bs:(i+1)*bs], lbs[i*bs:(i+1)*bs])
+    for s in names[1:]:
+        feats = np.load(f'{s}_features.npy')
+        lbs = np.load(f'{s}_labels.npy')
 
-model.fit(generate_data(),
-                    steps_per_epoch=10000, epochs=10)
-
-"""
-
-names = ['Lille1_1', 'Lille1_2', 'Lille2', 'Paris1', 'Paris2']
-
-for s in names:
-    feats = np.load(f'{s}_features.npy')
-    lbs = np.load(f'{s}_labels.npy')
+        features = np.vstack((features, feats))
+        labels = np.vstack((labels, lbs))
 
     # shuffle data
-    inds = np.random.shuffle(range(len(feats)))
-    feats = feats[inds]
-    lbs = lbs[inds]    
-    
-    # train on batches
-    batch_size = 64
-    
-    print(np.min(lbs), np.max(lbs))
-    print(feats.shape)
-    
-    print('starting train on batch')
-    model.train_on_batch(feats, lbs)
-    print('done')
+    inds = np.random.shuffle(range(len(features)))
+    features = features[inds]
+    labels = labels[inds] 
 
-
-    #features = np.vstack((features, feats))
-    #labels = np.vstack((labels, lbs))
-
-    del feats
-    del lbs
-"""
-
+    print('data loaded:', features.shape)   
+     
+    model.fit(features, labels)
 
