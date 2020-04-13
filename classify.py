16e7d67cfbd97d4244fe57f8b9babe24f3df0802
nathanlct <nathan.lichtle@gmail.com>
2020-04-13 23:16:46 +0200

    .

--- classify.py
+++ classify.py
@@ -10,13 +10,13 @@ for voxel_size in [0.05]:#, 0.05, 0.10, 0.15, 0.20, 0.4, 0.7, 1.0]:
     batch_size = 64
 
     model = tf.keras.Sequential([
-        tf.keras.layers.Dropout(0.25),
+        tf.keras.layers.Dropout(0.2),
         tf.keras.layers.Dense(32, activation='relu', activity_regularizer=tf.keras.regularizers.l1(0.01)),
-        tf.keras.layers.Dropout(0.4),
-        tf.keras.layers.Dense(7, activation='softmax', activity_regularizer=tf.keras.regularizers.l1(0.01))
+        tf.keras.layers.Dropout(0.35),
+        tf.keras.layers.Dense(7, activation='softmax', activity_regularizer=tf.keras.regularizers.l1(0.05))
     ])
 
-    model.compile(optimizer=tf.keras.optimizers.Adam(learning_rate=1e-3),
+    model.compile(optimizer=tf.keras.optimizers.Adam(learning_rate=1e-2),
                   loss=tf.keras.losses.SparseCategoricalCrossentropy(),#from_logits=True),
                   metrics=['sparse_categorical_accuracy'])
 
